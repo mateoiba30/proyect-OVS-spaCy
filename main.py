41048ff020915c8e30e7ff0f16b7070f6517beac
Mateo Ibanez <mateoiba30@gmail.com>
2024-04-15 21:56:25 -0300

    casi termino el patron direccion e incio el de fot

--- main.py
+++ main.py
@@ -9,56 +9,52 @@ nlp = spacy.load("es_core_news_lg")#importamos la info entrenada en español con
 gt = pd.read_csv("csvFile.csv")#abrimos el csv -> lo hacemos una DataFrame
 gt = gt.fillna("")#los datos nulos=incompletos no les asignamos texto
 
-rangeRows = gt.iloc[0:5]# no incluye el numero 5
+rangeRows = gt.iloc[0:16]# no incluye el numero 5
 
 for index, row in rangeRows.iterrows():#gracias a pandas, recorremos sencillamente el csv
 
     description = row["DESCRIPCION"]#pedimos una columna especifica segun el nombre de la columna, indicada en la 1er linea del csv
     doc = nlp(description)# el doc tiene cada palabra/token
 
-    #print(description) #para ver el texto normal, aunque lo podríamos ver en el edit-csv.net
     print("Descripcion numero ", index+1)
+    print("")
+    print(description) #para ver el texto normal, aunque lo podríamos ver en el edit-csv.net
+    print("")
     for token in doc: print(token.text, token.pos_, token.dep_, token.head.text) #para ver más a fondo la descripción de cada token
 
     matcher = Matcher(nlp.vocab)
-    matcher.add("adressPatterns", [
-        [{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": {"IN": ["ADP", "PROPN"]}, "OP":"*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y", "OP": "?"},{"POS": "PROPN", "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": {"IN": ["PROPN", "ADP"]}, "OP": "*"}]
-        #[{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": {"IN": ["ADP", "PROPN"]}, "OP":"*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y", "OP":"?"},{"POS": "PROPN", "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"}],
-        #[{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y", "OP":"?"},{"POS": "PROPN", "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"}],
-        #[{"TEXT": {"IN":["calle", "avenida", "ruta"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y", "OP":"?"},{"POS": {"IN":["PROPN", "NOUN"]}, "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"}],
-        #[{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"TEXT":"y", "OP":"?"},{"LIKE_NUM": True, "OP":"?"}]
+    matcher.add("fotPatterns", [
+            [{"LOWER": {"IN":["fot", "factor de ocupación del suelo"]}},{"POS": "ADJ", "OP":"?"},{"LIKE_NUM": True}]
+        ])#defino los patrones para encontrar cierto campo
 
-        ])#defino los patrones para encontrar la direccion
-
+    print("")
+    maxSize = 0
+    best_span = doc[0:0]
     print("matches:")
     matches = matcher(doc)#mostramos lo que encontramos con el patron
     for match_id, start, end in matches: #para lo que encontramos vamos a mostrar solo eso
-        matched_span = doc[start:end]  #hacemos un span = parte de la descripcion
-        print(matched_span.text)#mostramos el texto que coincide con el patron
+        print(doc[start:end].text)
+        actSize = end - start
+        if (actSize > maxSize):
+            maxSize = actSize
+            best_span = doc[start:end]
+
+    print("mejor matcheo:")
+    print(best_span.text)#mostramos el texto que coincide con el patron
 
     print("")
+    print("------------------------------------------------------------")
     print("")
 
 
-#matcher = Matcher(nlp.vocab)
-#matcher.add("patronesMateo", [
-#    #direccion
-#    [
+#    matcher.add("adressPatterns", [
+#        [{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": {"IN": ["ADP", "PROPN"]}, "OP":"+"},{"LIKE_NUM":True, "OP":"?"}],
+#        [{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": {"IN": ["ADP", "PROPN"]}, "OP":"*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y"},{"POS": "PROPN", "OP":"?"},{"LIKE_NUM": True, "OP":"+"}],
+#        [{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": {"IN": ["ADP", "PROPN"]}, "OP":"*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y"},{"POS": "PROPN", "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": "ADP", "OP": "*"},{"POS": "PROPN", "OP": "+"}],
 #
-#    ],
+#        #[{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": {"IN": ["ADP", "PROPN"]}, "OP":"*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y", "OP":"?"},{"POS": "PROPN", "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"}],
+#        #[{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y", "OP":"?"},{"POS": "PROPN", "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"}],
+#        #[{"TEXT": {"IN":["calle", "avenida", "ruta"]}},{"LIKE_NUM": True},{"POS":"ADJ", "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"},{"LIKE_NUM":True, "OP":"?"},{"TEXT":"y", "OP":"?"},{"POS": {"IN":["PROPN", "NOUN"]}, "OP":"?"},{"LIKE_NUM": True, "OP":"?"},{"POS": "ADP", "OP":"?"},{"POS": "PROPN", "OP": "*"}],
+#        #[{"POS": {"IN":["PROPN", "NOUN"]}},{"LIKE_NUM": True},{"TEXT":"y", "OP":"?"},{"LIKE_NUM": True, "OP":"?"}]
 #
-#    #fot
-#    [
-#
-#    ],
-#
-#    #barrio privado
-#    [
-#
-#    ],
-#
-#    #medidas terreno
-#    [
-#
-#    ]
-#])
+#        ])#defino los patrones para encontrar la direccion
