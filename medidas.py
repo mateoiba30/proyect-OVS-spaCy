d837acdbb786688417ee7d343dc464cf4b4aa8f2
Mateo Ibanez <mateoiba30@gmail.com>
2024-04-29 19:30:10 -0300

    agrego medidas para terrenos irregulares

--- medidas.py
+++ medidas.py
@@ -9,7 +9,7 @@ nlp = spacy.load("es_core_news_lg")#importamos la info entrenada en español con
 gt = pd.read_csv("csvFile.csv")#abrimos el csv -> lo hacemos una DataFrame
 gt = gt.fillna("")#los datos nulos=incompletos no les asignamos texto
 
-rangeRows = gt.iloc[9:12]# no incluye el numero 5
+rangeRows = gt.iloc[8:9]# 16:23
 
 for index, row in rangeRows.iterrows():#gracias a pandas, recorremos sencillamente el csv
 
@@ -20,14 +20,38 @@ for index, row in rangeRows.iterrows():#gracias a pandas, recorremos sencillamen
     print("")
     print(description) #para ver el texto normal, aunque lo podríamos ver en el edit-csv.net
     print("")
-    #for token in doc: print(token.text, token.pos_, token.dep_, token.head.text) #para ver más a fondo la descripción de cada token
+    for token in doc: print(token.text, token.pos_, token.dep_, token.head.text) #para ver más a fondo la descripción de cada token
 
-    conectores = ["x", "y", "por"]
+    dimension = ["frente", "fondo", "lateral", "ancho", "alto", "profundidad", "largo", "anchura", "longitud", "espesor"]
+    conectores = ["x", "y", "por", "de", "con"]
     medidas = ["metro", "m", "mt", "mts", "ms"] #el LEMMA no funciona con "m" porque cree que es un número en lugar de una palabra
+    relleno = ["ADP", "ADV", "PROPN", "NOUN", "DET", "ADJ"] # sacarle el DET y ADJ? -> por ahora da buenos resultados sin perjudicar
+    #principal = {"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": ["ADP", "ADV", "PROPN", "NOUN"]}, "OP": "*"}
+
     matcher = Matcher(nlp.vocab)
-    matcher.add("measuresPatterns", [
-            [{"LIKE_NUM":True},{"LEMMA": {"IN":medidas}}, {"POS":{"IN":["ADP", "ADV", "PROPN", "NOUN"]}, "OP":"*"},{"LOWER":{"IN":conectores}}, {"LIKE_NUM":True, "OP":"+"}, {"LOWER": {"IN":medidas}, "OP":"?"}],
-            [{"LIKE_NUM":True},{"LEMMA": {"IN":medidas}, "OP":"?"}, {"POS":{"IN":["ADP", "ADV","PROPN", "NOUN"]}, "OP":"*"},{"LOWER":{"IN":conectores}}, {"LIKE_NUM":True, "OP":"+"}, {"LOWER": {"IN":medidas}}],
+    matcher.add("measuresPatterns", [ 
+            #2 medidas:
+            [{"LIKE_NUM":True},{"LEMMA": {"IN":medidas}}, {"POS":{"IN":relleno}, "OP":"*"},{"LOWER":{"IN":conectores}}, {"LIKE_NUM":True, "OP":"+"}, {"LOWER": {"IN":medidas}, "OP":"?"}],
+            [{"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN":conectores}}, {"LIKE_NUM":True, "OP":"+"}, {"LOWER": {"IN":medidas}}],
+ 
+            #intentos fallidos:
+            #[{"LIKE_NUM": True}, {"POS":"PUNCT", "OP":"?"},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"?"}, {"POS":"PUNCT", "OP":"?"}, {"LIKE_NUM": True},{"POS":"PUNCT", "OP":"?"}, {"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"?"},{"POS": {"IN": relleno}, "OP": "*"}, {"POS":"PUNCT", "OP":"?"}, {"LIKE_NUM": True}],         
+            #[{"LEMMA": {"IN": dimension}},{"LOWER": "de"},{"LIKE_NUM":True},{"ORTH":",", "OP":"?"}, {"LEMMA":{"IN":relleno}, "POS":"?"},{"LEMMA": {"IN": dimension}},{"LOWER": "de"},{"LIKE_NUM":True},{"ORTH":",", "OP":"?"}, {"LEMMA":{"IN":relleno}, "POS":"?"},{"LEMMA": {"IN": dimension}},{"LOWER": "de"},{"LIKE_NUM":True},{"ORTH":",", "OP":"?"}],
+            
+            #3 medidas, lo hago varias veces para asegurarme que en algún lado se diga la medida
+            [{"LIKE_NUM": True},{"LEMMA": {"IN": medidas}}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"*"}, {"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}],
+            [{"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"*"}, {"LIKE_NUM": True},{"LEMMA": {"IN": medidas}}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}],
+            [{"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"*"}, {"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}}],
+
+
+            #4 medidas, que tenga la medida en algún lado
+            [{"LIKE_NUM": True},{"LEMMA": {"IN": medidas}}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"*"}, {"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}],     
+            [{"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"*"}, {"LIKE_NUM": True},{"LEMMA": {"IN": medidas}}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}],     
+            [{"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"*"}, {"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}],     
+            [{"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"},{"LOWER":{"IN": conectores}, "OP":"*"}, {"LIKE_NUM": True},{"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}, "OP":"?"}, {"POS": {"IN": relleno}, "OP": "*"}, {"LOWER": {"IN": conectores}, "OP":"*"},{"LIKE_NUM": True}, {"LEMMA": {"IN": medidas}}],     
+
+
+
         ])#defino los patrones para encontrar cierto campo
 
     print("")
