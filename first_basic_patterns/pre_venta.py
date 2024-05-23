2695cb9e43ec4f834b197ae85c3ad5413ab3fc87
Mateo Ibanez <mateoiba30@gmail.com>
2024-05-23 11:27:43 -0300

    tengo errores raros

--- first_basic_patterns/pre_venta.py
+++ first_basic_patterns/pre_venta.py
@@ -11,11 +11,6 @@ gt = gt.fillna("")
 
 rangeRows = gt.iloc[:27]
 
-TP = 0
-TN = 0
-FN = 0
-FP = 0
-
 for index, row in rangeRows.iterrows():#gracias a pandas, recorremos sencillamente el csv
 
     description = row["DESCRIPCION"]#pedimos una columna especifica segun el nombre de la columna, indicada en la 1er linea del csv
@@ -25,63 +20,39 @@ for index, row in rangeRows.iterrows():#gracias a pandas, recorremos sencillamen
     tiempo = ["año", "años", "mes", "meses"]
     #para usar con LOWER
     palabraPreVenta = ["preventa", "pre-venta"]
-    palabrasFuturoExactas = ["concretará", "contará","propuesta", "construcción", "proyecto", "entregará", "entregarán", "entrega", "posesión", "obra", "desarrollo"]
-
+    palabrasFuturoExactas = ["concretará", "contará","propuesta", "construcción", "proyecto", "entregará", "entregarán", "entrega", "posesion", "posesión", "obra", "desarrollo"]
 
     matcher = Matcher(nlp.vocab)
     matcher.add("preventa", [ 
             
+            #para ver si es preventa
             [{"LOWER":{"IN": palabraPreVenta}}],
             [{"LOWER": "pre"}, {"LOWER": "venta"}],
-            [{"LOWER": "posesión"}, {"LOWER": "futura"}],
-            [{"LOWER": "posesión"}, {"LOWER": "a"}, {"LIKE_NUM": True}, {"LOWER": {"IN": tiempo}}],
+
+            #para ver si se habla en futuro o similar
+            [{"LOWER":{"IN": palabrasFuturoExactas}}],
             [{"LOWER": "primeras"}, {"LIKE_NUM": True}, {"LOWER": "unidades"}],
 
-        ])#defino los patrones para encontrar cierto campo
+            #no necesarias porque ya me conformo solo con encontrar la palabra posesión
+            #[{"LOWER": "posesión"}, {"LOWER": "futura"}],
+            #[{"LOWER": "posesión"}, {"LOWER": "a"}, {"LIKE_NUM": True}, {"LOWER": {"IN": tiempo}}],
 
-    mostrar = False
+        ])#defino los patrones para encontrar cierto campo
+    
     prediccion = False
-    print("")
     matches = matcher(doc)#mostramos lo que encontramos con el patron
     if len(matches) > 0:
         prediccion: True
-        if resultado:
-            TP+=1
-        else:
-            FP+=1
-            mostrar = True
-        
-    else:
-        if not resultado:
-            TN+=1
-        else:
-            FN +=1
-            mostrar = True
 
-    if mostrar:
-        print(f"\nDescripcion numero", index+1)
-        print("")
-        print(description) #para ver el texto normal, aunque lo podríamos ver en el edit-csv.net
-        print("")
-        #for token in doc: print(token.text, token.pos_, token.dep_, token.head.text) #para ver más a fondo la descripción de cada token
-        print("")
-        print("esperado: " + str(resultado))
-        print("")
-        print("prediccion: " + str(prediccion))
-        print("")
-        print("------------------------------------------------------------")
-        print("")
-
-print("")
-print("----------------MEDIDAS----------------")
-print("")
-presicion = TP/(TP+FP)
-recall = TP/(TP+FN)
-f1 = 2*(presicion*recall)/(presicion+recall)
-print("tp: " + str(TP))
-print("fp: " + str(FP))
-print("fn: " + str(FN))
-print("tn: " + str(TN))
-print("precision: " + str(presicion))
-print("recall: " + str(recall))
-print("f1: " + str(f1))
+    print("------------------------------------------------------------")
+    print("")
+    print(f"\nDescripcion numero", index+1)
+    print("")
+    print(description) #para ver el texto normal, aunque lo podríamos ver en el edit-csv.net
+    print("")
+    #for token in doc: print(token.text, token.pos_, token.dep_, token.head.text) #para ver más a fondo la descripción de cada token    mostrar = False
+    print("")
+    print("esperado: " + str(resultado))
+    print("")
+    print("prediccion: " + str(prediccion))
+    print("")
