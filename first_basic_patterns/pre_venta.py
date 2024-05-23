6afc9830b3021d6475998ce38950e04a8f4ab7b2
Mateo Ibanez <mateoiba30@gmail.com>
2024-05-23 11:38:56 -0300

    arreglo el medidor de medidas

--- first_basic_patterns/pre_venta.py
+++ first_basic_patterns/pre_venta.py
@@ -11,6 +11,11 @@ gt = gt.fillna("")
 
 rangeRows = gt.iloc[:27]
 
+TP = 0
+TN = 0
+FN = 0
+FP = 0
+
 for index, row in rangeRows.iterrows():#gracias a pandas, recorremos sencillamente el csv
 
     description = row["DESCRIPCION"]#pedimos una columna especifica segun el nombre de la columna, indicada en la 1er linea del csv
@@ -38,27 +43,54 @@ for index, row in rangeRows.iterrows():#gracias a pandas, recorremos sencillamen
             #[{"LOWER": "posesión"}, {"LOWER": "a"}, {"LIKE_NUM": True}, {"LOWER": {"IN": tiempo}}],
 
         ])#defino los patrones para encontrar cierto campo
-    print("------------------------------------------------------------")
-    print("")
-    print(f"\nDescripcion numero", index+1)
-    print("")
-    print(description) #para ver el texto normal, aunque lo podríamos ver en el edit-csv.net
-    print("")
-    #for token in doc: print(token.text, token.pos_, token.dep_, token.head.text) #para ver más a fondo la descripción de cada token    mostrar = False
-    print("")
-    print("esperado: " + str(resultado))
-    print("")
-    print("matcheos: ")
+    mostrar = False
     matcheos = 0
     matches = matcher(doc)#mostramos lo que encontramos con el patron
     for match_id, start, end in matches: #para lo que encontramos vamos a mostrar solo eso
-        print(doc[start:end].text)
         matcheos += 1
     prediccion = False
     if matcheos > 0:
         prediccion = True
-    print("")
-    print("prediccion: " + str(prediccion))
-    print("")
+        if resultado:
+            TP+=1
+        else:
+            FP+=1
+            mostrar = True
+    else:
+        if not resultado:
+            TN+=1
+        else:
+            FN +=1
+            mostrar = True   
 
+    if mostrar:
+        print("------------------------------------------------------------")
+        print("")
+        print(f"\nDescripcion numero", index+1)
+        print("")
+        print(description) #para ver el texto normal, aunque lo podríamos ver en el edit-csv.net
+        print("")
+        #for token in doc: print(token.text, token.pos_, token.dep_, token.head.text) #para ver más a fondo la descripción de cada token    mostrar = False
+        print("")
+        print("esperado: " + str(resultado))
+        print("")
+        print("prediccion: " + str(prediccion))
+        print("")
+        print("matcheos: ")
+        for match_id, start, end in matches:
+            print(doc[start:end].text)
+        print("")
 
+print("")
+print("----------------MEDIDAS----------------")
+print("")
+presicion = TP/(TP+FP)
+recall = TP/(TP+FN)
+f1 = 2*(presicion*recall)/(presicion+recall)
+print("tp: " + str(TP))
+print("fp: " + str(FP))
+print("fn: " + str(FN))
+print("tn: " + str(TN))
+print("precision: " + str(presicion))
+print("recall: " + str(recall))
+print("f1: " + str(f1))
